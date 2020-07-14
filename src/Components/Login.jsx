c9f6e1daa3d8b61eeae1deb4ff5ca2e06a6b3786
oliverochman <oliver.ochman@hotmail.com>
2020-07-14 17:18:19 +0200

    session regarding registration

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,18 +5,29 @@ import LoginForm from "./LoginForm"
 
 class Login extends Component {
   state = {
-    renderForm: false
+    renderForm: false,
+    registration: false
   }
 
   authenticate = async (event) => {
     event.preventDefault()
     try {
-      let response = await axios.post("/auth", {
-        email: event.target.email.value,
-        password: event.target.password.value
 
-      })
+      let response
 
+      if (this.state.registration) {
+        response = await axios.post("/auth", {
+          email: event.target.email.value,
+          password: event.target.password.value,
+          password_confirmation: event.target.passwordConfirmation.value
+        })
+      } else {
+        response = await axios.post("/auth/sign_in", {
+          email: event.target.email.value,
+          password: event.target.password.value
+        })
+      }
+      
       await storeAuthCredentials(response)
       this.props.setAuthenticated()
     } catch (error) {
@@ -29,7 +40,10 @@ class Login extends Component {
     let form
 
     this.state.renderForm ? (
-      form = <LoginForm authenticate={this.authenticate} />
+      form = <LoginForm 
+              authenticate={this.authenticate} 
+              registration={this.state.registration}
+              toggleRegistration={() => this.setState({registration: !this.state.registration})}/>
     ) : (
         button = <button id="login" onClick={() => this.setState({ renderForm: true })}>Login</button>
       )
--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
 const LoginForm = (props) => {
+  let toggleRegistrationElement = props.registration ? 'Do you already have an account' : 'Sign up!'
   return (
-    <form onSubmit={props.authenticate} id='login-form'>
-      <label>Email</label>
-      <input name='email' type='email' id='email' />
-      <label>Password</label>
-      <input name='password' type='password' id='password' />
-      <button id= 'submit'>Submit</button>
-    </form>
+    <>
+      <form onSubmit={props.authenticate} id='login-form'>
+        <label>Email</label>
+        <input name='email' type='email' id='email' />
+        <label>Password</label>
+        <input name='password' type='password' id='password' />
+        {
+          props.registration && (
+            <>
+              <label>Password confirmation</label>
+              <input name='passwordConfirmation' type='password' id='passwordConfirmation' />
+            </>
+          )
+        }
+
+        <button id='submit'>Submit</button>
+      </form>
+
+      <button onClick={props.toggleRegistration}>{toggleRegistrationElement}</button>
+    </>
   )
 }
 
